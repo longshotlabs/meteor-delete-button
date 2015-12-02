fb5cbde58598ad829857036182f15b6882936725
Eric Dobbertin <eric@dairystatedesigns.com>
2015-12-02 10:42:08 -0600

    Remove pre-Meteor1.0 compatibility and publish as 2.0

--- package.js
+++ package.js
@@ -1,17 +1,13 @@
 Package.describe({
   name: "aldeed:delete-button",
-  version: "1.0.1",
+  version: "2.0.0",
   summary: "Provides a delete button UI component",
   git: "https://github.com/aldeed/meteor-delete-button.git"
 });
 
-Package.on_use(function(api) {
-  if (api.versionsFrom) {
-    api.versionsFrom('METEOR@0.9.1');
-    api.use(['templating', 'mongo']);
-  } else {
-    api.use(['templating', 'mongo-livedata']);
-  }
+Package.onUse(function(api) {
+  api.versionsFrom('METEOR@1.0');
+  api.use(['templating', 'mongo']);
 
-  api.add_files(['delete-button.html', 'delete-button.js'], 'client');
+  api.addFiles(['delete-button.html', 'delete-button.js'], 'client');
 });
